c4ca058f5cb4840f95c0c50d8e708650f597cb8d
Nathan Lehman <nlehman@dlcouch.com>
2020-12-16 13:27:51 -0500

    vue fixes

--- src/main/resources/js/components/Product.vue
+++ src/main/resources/js/components/Product.vue
@@ -1,7 +1,7 @@
 <template>
-  <header class="bg-white dark:bg-gray-700 shadow">
+  <header class="bg-white shadow dark:bg-gray-700">
     <div
-      class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8 flex items-center justify-between"
+      class="flex items-center justify-between px-4 py-6 mx-auto max-w-7xl sm:px-6 lg:px-8"
     >
       <h1
         class="text-3xl font-bold leading-tight text-gray-900 dark:text-gray-300"
@@ -10,7 +10,7 @@
       </h1>
       <router-link
         to="/products"
-        class="p-3 rounded-md text-base font-medium text-gray-900 dark:text-gray-200 hover:bg-gray-100 dark:hover:bg-gray-500"
+        class="p-3 text-base font-medium text-gray-900 rounded-md dark:text-gray-200 hover:bg-gray-100 dark:hover:bg-gray-500"
         >Browse Products</router-link
       >
     </div>
@@ -19,11 +19,12 @@
 
 <script>
 export default {
-    data() {
-        return {
-            product: null
-        }
-    },
+  name: "Product",
+  data() {
+    return {
+      product: {},
+    };
+  },
   computed: {
     productId: function () {
       return this.$route.params.productId;
@@ -31,7 +32,6 @@ export default {
   },
   methods: {
     loadProduct: function () {
-        console.log('here')
       axios
         .get(`/products/${this.productId}`)
         .then((response) => {
